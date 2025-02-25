d10afc38d811ff5e8b316b0b7d0ab1ca2caf6fa1
MasterHacker10 <quinnsidell@icloud.com>
2025-02-25 18:06:03 -0500

    hi
    i did things

---
+++ Sketch Head/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Target Object")]
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.position.y > transform.position.y) {
+            transform.position = new Vector3(0, target.position.y, -10f);
+        }
+    }
+}
